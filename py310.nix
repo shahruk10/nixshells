62a24a0c424497b8c30356493c4500dd95202878
Shahruk Hossain <shahruk10@gmail.com>
2026-02-02 13:03:19 +0600

    feat!: use nix flakes

--- py310.nix
+++
@@ -1,9 +0,0 @@
-# Author: Shahruk Hossain <shahruk10@gmail.com>
-# Year: 2022
-#
-# py310.nix - Python 3.10.x environment.
-
-import ./pycuda_common.nix {
-  python_version = "310";
-  gcc_version = "10";
-}
